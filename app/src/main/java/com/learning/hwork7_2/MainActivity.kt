a1c51f76deaa53d55a5a710de7ebc334f1d2de7c
zahramarri <zahramarri.dev@gmail.com>
2022-06-28 16:29:09 +0430

    handle isClickable again, set onClickListener for buttons previous and next

--- app/src/main/java/com/learning/hwork7_2/MainActivity.kt
+++ app/src/main/java/com/learning/hwork7_2/MainActivity.kt
@@ -6,29 +6,50 @@ import android.widget.Toast
 import com.learning.hwork7_2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
+    private var indexOfQuestion = 0
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.textViewQuestion.text = QuizContent.listOfQuestions[0]
+        binding.textViewQuestion.text = QuizContent.listOfQuestions[indexOfQuestion]
+
+        binding.buttonPrevious.setOnClickListener{
+            indexOfQuestion --
+            binding.textViewQuestion.text = QuizContent.listOfQuestions[indexOfQuestion]
+            if (QuizContent.isAnswered[QuizContent.listOfQuestions[indexOfQuestion]] == false) {
+                binding.buttonTrue.isClickable = true
+                binding.buttonFalse.isClickable = true
+            }
+        }
+
+        binding.buttonNext.setOnClickListener{
+            indexOfQuestion ++
+            binding.textViewQuestion.text = QuizContent.listOfQuestions[indexOfQuestion]
+            if (QuizContent.isAnswered[QuizContent.listOfQuestions[indexOfQuestion]] == false) {
+                binding.buttonTrue.isClickable = true
+                binding.buttonFalse.isClickable = true
+            }
+        }
 
         binding.buttonTrue.setOnClickListener{
-            if (binding.buttonTrue.text.toString().toBoolean() == QuizContent.questionsAndAnswers[binding.textViewQuestion.text]) {
+            if (binding.buttonTrue.text.toString().toBoolean() == QuizContent.answers[binding.textViewQuestion.text]) {
                 Toast.makeText(this, "CORRECT", Toast.LENGTH_SHORT).show()
             } else {
                 Toast.makeText(this, "INCORRECT", Toast.LENGTH_SHORT).show()
             }
+            QuizContent.isAnswered[QuizContent.listOfQuestions[indexOfQuestion]] = true
             it.isClickable = false
             binding.buttonFalse.isClickable = false
         }
         binding.buttonFalse.setOnClickListener{
-            if (binding.buttonFalse.text.toString().toBoolean() == QuizContent.questionsAndAnswers[binding.textViewQuestion.text]) {
+            if (binding.buttonFalse.text.toString().toBoolean() == QuizContent.answers[binding.textViewQuestion.text]) {
                 Toast.makeText(this, "CORRECT", Toast.LENGTH_SHORT).show()
             } else {
                 Toast.makeText(this, "INCORRECT", Toast.LENGTH_SHORT).show()
             }
+            QuizContent.isAnswered[QuizContent.listOfQuestions[indexOfQuestion]] = true
             it.isClickable = false
             binding.buttonTrue.isClickable = false
         }
