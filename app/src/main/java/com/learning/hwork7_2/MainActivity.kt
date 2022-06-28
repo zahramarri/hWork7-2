3af3eb40ebd0d3ca21328d06b90346fdbee16d97
zahramarri <zahramarri.dev@gmail.com>
2022-06-28 16:06:05 +0430

    add QuizContent object and set onClickListener for true and false buttons

--- app/src/main/java/com/learning/hwork7_2/MainActivity.kt
+++ app/src/main/java/com/learning/hwork7_2/MainActivity.kt
@@ -2,10 +2,31 @@ package com.learning.hwork7_2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.learning.hwork7_2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.textViewQuestion.text = QuizContent.listOfQuestions[0]
+
+        binding.buttonTrue.setOnClickListener{
+            if (binding.buttonTrue.text.toString().toBoolean() == QuizContent.questionsAndAnswers[binding.textViewQuestion.text]) {
+                Toast.makeText(this, "CORRECT", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "INCORRECT", Toast.LENGTH_SHORT).show()
+            }
+        }
+        binding.buttonFalse.setOnClickListener{
+            if (binding.buttonFalse.text.toString().toBoolean() == QuizContent.questionsAndAnswers[binding.textViewQuestion.text]) {
+                Toast.makeText(this, "CORRECT", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "INCORRECT", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/learning/hwork7_2/QuizContent.kt
@@ -0,0 +1,27 @@
+package com.learning.hwork7_2
+
+object QuizContent {
+    val questionsAndAnswers: MutableMap<String, Boolean> = mutableMapOf(
+        "There are three rivers in Saudi Arabia." to false,
+        "The Great Wall of China is visible from space." to false,
+        "The Atlantic Ocean is the warmest in the world." to false,
+        "When the Eiffel Tower was unveiled, the Parisian art community hated it." to true,
+        "There are more ancient pyramids in Sudan than in Egypt." to true,
+        "Janet Jackson performed at halftime of Super Bowl LV." to false,
+        "The Los Angeles Lakers won the 2020 NBA Championship." to true,
+        "Hamilton the musical is the first Broadway show ever written about Hamilton." to false,
+        "The film Moneyball is based on a true story." to true,
+        "OKRs are a collaborative goal-setting method used to set challenging goals with measurable results." to true)
+
+    val listOfQuestions: MutableList<String> = mutableListOf(
+        "There are three rivers in Saudi Arabia.",
+        "The Great Wall of China is visible from space.",
+        "The Atlantic Ocean is the warmest in the world.",
+        "When the Eiffel Tower was unveiled, the Parisian art community hated it.",
+        "There are more ancient pyramids in Sudan than in Egypt.",
+        "Janet Jackson performed at halftime of Super Bowl LV.",
+        "The Los Angeles Lakers won the 2020 NBA Championship.",
+        "Hamilton the musical is the first Broadway show ever written about Hamilton.",
+        "The film Moneyball is based on a true story.",
+        "OKRs are a collaborative goal-setting method used to set challenging goals with measurable results.")
+}
